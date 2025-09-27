f4aa38aeb92d91526aa091305a46191008898f69
Immanuel Kinuthia <immanuel.kinuthia@student.moringaschool.com>
2025-09-27 11:32:19 +0300

    Seed User

--- server/seed.py
+++ server/seed.py
@@ -6,3 +6,9 @@ with app.app_context():
     db.drop_all()
     db.create_all()
 
+mentor = User(name="Mentor One", email="mentor@example.com", password=generate_password_hash("password"), role="mentor")
+student = User(name="Student One", email="student@example.com", password=generate_password_hash("password"), role="student")
+db.session.add(mentor)
+db.session.add(student)
+db.session.commit()
+
