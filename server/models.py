d52ed40bab9f768e9ca3b4173257c5ad9673f4cd
Immanuel Kinuthia <immanuel.kinuthia@student.moringaschool.com>
2025-09-27 11:25:48 +0300

    User

--- server/models.py
+++ server/models.py
@@ -1,5 +1,17 @@
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy_serializer import SerializerMixin
 
-
 db = SQLAlchemy()
+
+class User(db.Model, SerializerMixin):
+    serialize_rules = ('-courses', '-enrollments')
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(100), unique=True, nullable=False)
+    password = db.Column(db.String(100), nullable=False)
+    role = db.Column(db.String(20), nullable=False, default='student')
+
+    courses = db.relationship('Course', backref='creator', lazy=True)
+    enrollments = db.relationship('Enrollment', backref='user', lazy=True)
+
