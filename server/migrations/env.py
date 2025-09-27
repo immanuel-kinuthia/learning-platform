6873f75293f1a166f0ab153cc3fddd5b0899bce4
Immanuel Kinuthia <immanuel.kinuthia@student.moringaschool.com>
2025-09-27 11:43:46 +0300

    get_engine

--- server/migrations/env.py
+++ server/migrations/env.py
@@ -8,3 +8,9 @@ config = context.config
 fileConfig(config.config_file_name)
 logger = logging.getLogger('alembic.env')
 
+def get_engine():
+    try:
+        return current_app.extensions['migrate'].db.get_engine()
+    except (TypeError, AttributeError):
+        return current_app.extensions['migrate'].db.engine
+    
